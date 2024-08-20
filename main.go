d685f0665b091c04e34d8fa27c28b9d5c4d1d70d
thomas21-jquerry <thomasjose875@gmail.com>
2024-08-20 23:46:49 +0530

    boiler plate done

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
 
 func main(){
 	fmt.Println("hello world");
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT");
+	if port == "" {
+		log.Fatal("Port not found");
+	} else {
+		fmt.Println("PORT: ", port);
+	}
 }
