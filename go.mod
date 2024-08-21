5a51a45e12b0cf4d8ff6c85eec77897a42bb98b5
thomas21-jquerry <thomasjose875@gmail.com>
2024-08-21 20:02:02 +0530

    boilerplate for http done'

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/thomas21-jquerry/rssagg
 
 go 1.23.0
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main(){
-	fmt.Println("hello world");
 	godotenv.Load(".env")
 
 	port := os.Getenv("PORT");
@@ -16,5 +19,31 @@ func main(){
 		log.Fatal("Port not found");
 	} else {
 		fmt.Println("PORT: ", port);
+	}
+	router := chi.NewRouter();
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*", },
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	v1Router := chi.NewRouter();
+	v1Router.Get("/err", handleError)
+	v1Router.Get("/healthz", handleReadiness)
+	router.Mount("/v1", v1Router)
+
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + port,
+	}
+	fmt.Printf("server starting on port : %v", port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
